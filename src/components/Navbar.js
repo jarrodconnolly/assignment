0ed24409166943f1a81e69ee707857baf823280f
Jarrod Connolly <jarrod@nestedquotes.ca>
2018-07-03 01:08:22 -0700

    Remove unused files. CSS fix for nav title.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import React from 'react';
 const NavBar = () => {
   return (
     <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-      <a className="navbar-brand col-sm-3 col-md-2 mr-0">Assignment</a>
+      <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="/">Assignment</a>
       <ul className="navbar-nav px-3">
         <li className="nav-item text-nowrap">
           <a className="nav-link">Sign out</a>
